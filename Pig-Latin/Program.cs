67f2c74cf38aca7f8f05e51bc18093033774f530
Eric Holt <fatmoti@gmail.com>
2019-04-26 16:46:30 -0400

    Update regex

--- Pig-Latin/Program.cs
+++ Pig-Latin/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Pig_Latin
@@ -10,11 +11,49 @@ namespace Pig_Latin
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(TranslateToPigLatin("Translate."));
+            Console.WriteLine(TranslateToPigLatin("united"));
+            string userInput = Console.ReadLine();
+
+            if (userInput.Trim().Contains(" "))
+            {
+                string[] sentenceSplit = userInput.Split(' ');
+                string translatedSentence = "";
+                foreach (string item in sentenceSplit)
+                {
+                    translatedSentence += TranslateToPigLatin(item) + " ";
+                }
+                Console.WriteLine(translatedSentence.Trim());
+            }
+            else
+            {
+                Console.WriteLine(TranslateToPigLatin(userInput));
+            }
         }
 
         public static string TranslateToPigLatin(string word)
         {
-            
+            if (Regex.IsMatch(word, @"^[a-zA-Z.!?]+$"))
+            {
+
+
+                char[] vowelsLower = { 'a', 'e', 'i', 'o', 'u' };
+                string[] res = Regex.Split(word, "[aeiouAEIOU]");
+
+                foreach (char letter in vowelsLower)
+                {
+                    if (word[0] == letter)
+                    {
+                        return word + "way";
+                    }
+                }
+
+                return word.Substring(res[0].Length) + res[0] + "ay";
+            }
+            else
+            {
+                return word;            
+            }
         }
 
     }
