b82916fcfada2eb529288560c1229b0133fe25df
Eric Holt <fatmoti@gmail.com>
2019-04-28 13:54:31 -0400

    Fix a couple of minor bugs and add comments

--- Pig-Latin/Program.cs
+++ Pig-Latin/Program.cs
@@ -14,28 +14,34 @@ namespace Pig_Latin
             {
                 Console.Write("Enter a line to be translated: ");
                 string userInput = Console.ReadLine();
-                if (userInput.Length < 0)
+
+                if (userInput.Trim().Length < 1)//Check if user actually entered anything besides spaces
                 {
                     Console.Clear();
                     Console.WriteLine("Your nothing translates into: ");
                 }
-                else if (userInput.Trim().Contains(" "))
+                else if (userInput.Trim().Contains(" "))//Checks if the input contains spaces in middle of string
                 {
                     Console.Clear();
                     Console.WriteLine("Your sentence translation");
-                    Console.WriteLine(TranslateSentenceToPigLatin(userInput));
+                    Console.WriteLine(TranslateSentenceToPigLatin(userInput));//String is a sentence so we call SentenceTranslator to do individual words in the sentence
                 }
                 else
                 {
                     Console.Clear();
                     Console.WriteLine("Your word translation: ");
-                    Console.WriteLine(TranslateWordToPigLatin(userInput));
+                    Console.WriteLine(TranslateWordToPigLatin(userInput));// Its just a word so translate it.
                 }
                 Console.WriteLine();
-                userStillTranslating = Continue("Continue Translating? y/n");
+                userStillTranslating = Continue("Continue Translating? y/n");//See if the user wants to translate again
             }
         }
 
+        /// <summary>
+        /// Takes a string and returns a string array. Splits string based on whitespace
+        /// </summary>
+        /// <param name="sentence">string</param>
+        /// <returns>string array of sentence split by whitespace</returns>
         private static string[] SplitSentence(string sentence)
         {
             return sentence.Split(' ');
@@ -51,27 +57,32 @@ namespace Pig_Latin
 
             return sentence;
         }
+
+        /// <summary>
+        /// Translate a single word into pig latin if it doesn't contain a number or non punctuation symbols
+        /// </summary>
+        /// <param name="word">string</param>
+        /// <returns>string of either a translated word or just the orginal word if it doesn't meet requirements</returns>
         public static string TranslateWordToPigLatin(string word)
         {
-            if (Regex.IsMatch(word, @"^[a-zA-Z.!?']+$"))
+            if (Regex.IsMatch(word, @"^[a-zA-Z.!?']+$"))//If the word contains only letters or any number the following symbols .!?'
             {
-                if (Regex.IsMatch(word, @"^[.!?']+$"))
+                if (Regex.IsMatch(word, @"^[.!?']+$"))//If the word is only contains any number of allowed symbols then just return that word.
                 {
                     return word;
                 }
 
-                char[] vowelsLower = { 'a', 'e', 'i', 'o', 'u' };
-                string[] res = Regex.Split(word, "[aeiouAEIOU]");
-
-                foreach (char letter in vowelsLower)
+                char[] vowelsLower = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };                
+                foreach (char letter in vowelsLower)//Iterate through vowels array
                 {
-                    if (word[0] == letter)
+                    if (word[0] == letter)//Compare first letter of word for a vowel
                     {
                         return word + "way";
                     }
                 }
 
-                return word.Substring(res[0].Length) + res[0] + "ay";
+                string[] wordArraySplitByVowel = Regex.Split(word, "[aeiouAEIOU]");
+                return word.Substring(wordArraySplitByVowel[0].Length) + wordArraySplitByVowel[0] + "ay";
             }
             else
             {
